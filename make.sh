9f9998c26e856aa6ec74a7ead9f761482e099efb
alyx <oss@alyx.at>
2024-11-02 22:37:57 +0000

    Use local melange instance. Update to include pipelines directory.

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@ if [ "$#" -ne 1 ]; then
     echo "Usage $0 <package name>"
     exit 1
 fi
-docker run --privileged --rm -v "${PWD}":/work   cgr.dev/chainguard/melange build --signing-key melange.rsa pkg/$1.yml
+~/go/bin/melange build --signing-key melange.rsa --pipeline-dir=./pipelines pkg/$1.yml
